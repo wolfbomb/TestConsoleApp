45c55d9515a84e92f139bd5fd71d71ca58c2e8b5
wolfbomb <nimrod.resulta@gmail.com>
2017-07-14 21:56:51 +0800

    Added some codes.

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -10,6 +10,8 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
